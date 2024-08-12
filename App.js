db11610ce01fb37e7f1de6b1088bfc01fdb18849
mirazeng <zeng.wenqi@northeastern.edu>
2024-08-11 23:15:16 -0400

    update app.js

--- App.js
+++ App.js
@@ -12,10 +12,18 @@ const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.
 mongoose.connect(CONNECTION_STRING);
 
 app.use(cors({
-    credentials: true, origin: process.env.NETLIFY_URL || "http://localhost:3000"
+    credentials: true,
+    origin: process.env.NETLIFY_URL || "http://localhost:3000",
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 const sessionOptions = {
-    secret: process.env.SESSION_SECRET || "sparksecret", resave: false, saveUninitialized: false,
+    secret: process.env.SESSION_SECRET || "sparksecret", resave: false, saveUninitialized: false, cookie: {
+        sameSite: process.env.NODE_ENV === "production" ? 'none' : 'lax',
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 5 * 60 * 1000 // 1000ms is 1s. This is five minutes.
+    }
+
 };
 
 if (process.env.NODE_ENV !== "development") {
@@ -31,5 +39,4 @@ app.use(session(sessionOptions));
 app.use(express.json());
 const port = process.env.PORT || 4000;
 UserRoutes(app);
-
 app.listen(port);
